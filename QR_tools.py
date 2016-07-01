bb4517c074276e1ed6c547412b9a87a9338baacb
Sopetajo <castroduplessis@gmail.com>
2016-07-01 17:54:22 +0200

    Init QR_tools

---
+++ QR_tools.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# pip install pyqrcode
+# pip install qrtools
+
+import pyqrcode
+import qrtools.qrtools as qtools
+
+
+def print_qr_data(filepath):
+	qr = qtools.QR()
+	qr.decode(filepath)
+	print(qr.data)
+
+
+print_qr_data("qr_test.png")
