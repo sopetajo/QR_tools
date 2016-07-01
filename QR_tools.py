95b06aacb0d4b0082b5f9aad45853522b2b30825
root <root@localhost.localdomain>
2016-07-01 18:16:50 +0200

    Add dependency

--- QR_tools.py
+++ QR_tools.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # sudo apt-get install libzbar-dev
+# pip install pypng
 # pip install pyqrcode
 # pip install qrtools
 
@@ -14,4 +15,6 @@ def print_qr_data(filepath):
 	print(qr.data)
 
 
+qr = pyqrcode.create("Sopetajo")
+qr.png("qr_test.png", scale=6)
 print_qr_data("qr_test.png")
